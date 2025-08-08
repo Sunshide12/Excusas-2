a1de28d0c8bc14fa421456c911cc98c6fab6865a
Steban Martínez <stebanbusiness@gmail.com>
2025-08-07 23:39:27 -0500

    .

--- Modules/Estudiantes/js/main.js
+++ Modules/Estudiantes/js/main.js
@@ -88,42 +88,6 @@ function cargarContenido(seccion) {
     cargarCursosEstudiante();
 
     //Alera sobre los 5 dias de plazo para registrar la excusa:
-<<<<<<< HEAD
-setTimeout(() => {
-  const fechaInput = document.getElementById('fecha');
-  const mensajeFecha = document.getElementById('mensajeFecha');
-
-  if (fechaInput) {
-    fechaInput.addEventListener('change', function () {
-      const fechaSeleccionada = new Date(this.value);
-      const hoy = new Date();
-
-      fechaSeleccionada.setHours(0, 0, 0, 0);
-      hoy.setHours(0, 0, 0, 0);
-
-      let fechaTemp = new Date(fechaSeleccionada);
-      let diasHabiles = 0;
-
-      while (fechaTemp < hoy) {
-        const dia = fechaTemp.getDay();
-        if (dia !== 0 && dia !== 6) diasHabiles++;
-        fechaTemp.setDate(fechaTemp.getDate() + 1);
-      }
-
-      if (diasHabiles > 5) {
-        this.style.border = '2px solid red';
-        mensajeFecha.textContent =
-          '⚠️ Ha seleccionado una fecha con más de 5 días hábiles de antigüedad. La excusa podría no ser aprobada.';
-      } else {
-        this.style.border = '';
-        mensajeFecha.textContent = '';
-      }
-    });
-  }
-}, 0);
-
-
-=======
     setTimeout(() => {
       const fechaInput = document.getElementById("fecha");
       const mensajeFecha = document.getElementById("mensajeFecha");
@@ -156,7 +120,6 @@ setTimeout(() => {
         });
       }
     }, 0);
->>>>>>> 12c2302ef55095a6ac025080a6a15e65f5a50966
 
     // Mostrar el modal de advertencia
     $("#modalAdvertenciaExcusa").modal("show");
@@ -221,9 +184,6 @@ setTimeout(() => {
 
         const extension = archivo.name.split(".").pop().toLowerCase();
 
-<<<<<<< HEAD
-        if (extension !== "pdf") {
-=======
         if (
           extension !== "pdf" &&
           extension !== "jpg" &&
@@ -231,7 +191,6 @@ setTimeout(() => {
           extension !== "png" &&
           extension !== "zip"
         ) {
->>>>>>> 12c2302ef55095a6ac025080a6a15e65f5a50966
           alert(
             "❌ Solo se permiten archivos en formato PDF. Por favor seleccione un archivo válido."
           );
@@ -322,7 +281,7 @@ setTimeout(() => {
             alert("No se recibieron respuestas del servidor.");
           }
           limpiarFormulario();
-          
+
         } catch (error) {
           console.error("Error general:", error);
           alert(
